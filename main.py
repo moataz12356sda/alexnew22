2c380abd5d1c1a9d06be66221ea10c2cf0f92cdc
moataz12356sda <160052924+moataz12356sda@users.noreply.github.com>
2026-01-21 14:54:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ sensorlist = ['62212252', '62212257' ,'62211499', '62211501', '62211506', '62211
 def ConvertKSA (packet) :
     hour = packet[46:48]
     print(int(hour, 16))
-    newtime = str(hex(int(hour, 16) + 1)).replace("0x", "")
+    newtime = str(hex(int(hour, 16))).replace("0x", "")
     if len(newtime) == 1:
         newtime = "0" + newtime
     newpacket = packet[:46] + newtime + packet[48:]
@@ -38,7 +38,7 @@ def ConvertKSA (packet) :
 
 def Checked_SavedHolding_Database():
     client = InfluxDBClient(DATABASE_IP, DATABASE_PORT, USERNAME_DATABASE, PASSWORD_DATABASE, INTERNAL_BACKUP_DATABASE_NAME)
-    result = client.query('SELECT *  FROM "Hold"."autogen"."mostord" ')
+    result = client.query('SELECT *  FROM "Hold"."autogen"."Tzone" ')
     length = len(list(result.get_points()))
     if length != 0 :
         return True
@@ -46,11 +46,11 @@ def Checked_SavedHolding_Database():
         return False
 def Send_Saved_Database ():
     client = InfluxDBClient(DATABASE_IP, DATABASE_PORT, USERNAME_DATABASE, PASSWORD_DATABASE, INTERNAL_BACKUP_DATABASE_NAME)
-    result = client.query('SELECT *  FROM "Hold"."autogen"."mostord" ')
+    result = client.query('SELECT *  FROM "Hold"."autogen"."Tzone" ')
     data = list(result.get_points())
     for point in data :
         SendPacketToServer(str(point["Packet"]))
-        client.delete_series(database="Hold", measurement="mostord", tags={"id":point["id"]})
+        client.delete_series(database="Hold", measurement="Tzone", tags={"id":point["id"]})
 def Save_IndexNum(index) :
     textfile = open("IndexNum.txt", "w")
     textfile.write(str (index))
@@ -73,7 +73,7 @@ def SendPacketHoldingDataBase(packet) :
 
     DataPoint = [
         {
-            "measurement": "mostord",
+            "measurement": "Tzone",
             "tags" : {
                 "id": index
             },
@@ -344,3 +344,4 @@ class EchoServer(asyncore.dispatcher):
 
 server = EchoServer('', 2000)
 asyncore.loop()
+
